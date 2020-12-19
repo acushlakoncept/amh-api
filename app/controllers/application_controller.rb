e6895dd10f07a060e5e9963bc35e3eeb30d4e359
Uduak Essien <acushla4real@gmail.com>
2020-12-19 16:19:06 +0100

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-    
-    def render_jsonapi_response(resource)
-        if resource.errors.empty?
-            render jsonapi: resource
-        else
-            render jsonapi_errors: resource.errors, status: 400
-        end
+  def render_jsonapi_response(resource)
+    if resource.errors.empty?
+      render jsonapi: resource
+    else
+      render jsonapi_errors: resource.errors, status: 400
     end
+  end
 end
