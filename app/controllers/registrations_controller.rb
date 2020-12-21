9bc17bd99d24a32021a8f1955a7c90b259546718
Uduak Essien <acushla4real@gmail.com>
2020-12-21 17:23:28 +0100

    Add username signup key to devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def create
     build_resource(sign_up_params)
     resource.save
     sign_up(resource_name, resource) if resource.persisted?
 
     render_jsonapi_response(resource)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
