10e66148bd3260c565e106dc63dfc1a78b51199f
Uduak Essien <acushla4real@gmail.com>
2020-12-21 17:24:02 +0100

    Update serializable with username attributes

--- app/serializable/SerializableUser.rb
+++ app/serializable/SerializableUser.rb
@@ -3,7 +3,7 @@
 class SerializableUser < JSONAPI::Serializable::Resource
   type 'users'
 
-  attributes :email
+  attributes :username, :email
 
   link :self do
     @url_helpers.api_user_url(@object.id)
