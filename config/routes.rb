e4b6ace124846e846c5a2de439afa3c1093fa421
Uduak Essien <acushla4real@gmail.com>
2020-12-21 17:25:20 +0100

    Add index routes and hirb gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :users, only: %w[show]
+    resources :users, only: %w[show index]
   end
 
   devise_for :users,
